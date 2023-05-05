0f865666396d4a8844b9215709094db25591526a
Vladyslav Vronskyi <slava.pro1605@gmail.com>
2023-05-05 15:20:22 +0300

    Dal added

---
+++ Project/wwwroot/js/app.jsx
@@ -0,0 +1,9 @@
+class Hello extends React.Component {
+    render() {
+        return <h2>React.JS</h2>;
+    }
+}
+ReactDOM.render(
+    <Hello />,
+    document.getElementById("s1s")
+);
---
+++ SB.DAL/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SB.DAL.Interfaces
+{
+		public interface IRepository<TDbModel> where TDbModel : class
+		{
+			public List<TDbModel> GetAll();
+			public TDbModel Get(Guid id);
+			public void Delete(Guid id);
+			public TDbModel Update(TDbModel model);
+			public TDbModel Add(TDbModel model);
+		}
+	
+}
