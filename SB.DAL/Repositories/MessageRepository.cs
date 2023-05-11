1bc21740bd84f9420c9b2a78dafab6b74750c561
Vladyslav Vronskyi <slava.pro1605@gmail.com>
2023-05-11 14:54:24 +0300

    pj upd

--- SB.DAL/Repositories/MessageRepository.cs
+++ SB.DAL/Repositories/MessageRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SB.DAL.Interfaces;
+using SB.DAL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,31 @@ using System.Threading.Tasks;
 
 namespace SB.DAL.Repositories
 {
-	internal class MessageRepository
+	public class MessageRepository : IRepository<Message>
 	{
+		public Message Add(Message model)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Delete(string email)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Message Get(string email)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<Message> GetAll()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Message Update(Message model)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
